674d40505b4c2cfccc71f3bd4a256b73f91f3c40
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-08-17 18:51:17 -0300

    Add S3 Params when uploading files.

--- src/index.js
+++ src/index.js
@@ -53,7 +53,8 @@ class Service {
 
     fromBuffer(buffer)
     .pipe(this.Model.createWriteStream({
-      key: id
+      key: id,
+      params: params.query
     }, function () {
       cb(null, {
         [this.id]: id,
